1a107bfac158e7c712f926f751a923fa0da8df3e
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-01 09:29:50 -0500

    test: :white_check_mark: Testing, ejercicio 6
    Se realizó una configuración para poder testiar coorectamente el ejercicio, el cual pasó el testing sin problemas

--- tests/taller1/ejercicios.test.js
+++ tests/taller1/ejercicios.test.js
@@ -1,19 +1,18 @@
-import { punto5 } from "../../taller1/ejercicios";
+import { punto6 } from "../../taller1/ejercicios";
 
-describe('test punto 5', () => { 
-    test('lea por teclado dos números, si el primero es mayor al segundo informar su suma y diferencia', () => { 
-       let primero = 7
-       let segundo = 5
-       let result = punto5(primero, segundo)
-       expect(primero).toBeGreaterThan(segundo);
+describe('test punto 6', () => { 
+    test('Construir el algoritmo que lea por teclado dos números, si el primero es mayor al segundo informar su suma y diferencia, en caso contrario, informar el producto y la división del primero respecto al segundo.', () => { 
+      const estudiantes = [
+        { nombre: "Angela", nota: 70, sexo: "F" },
+        { nombre: "Saul", nota: 85, sexo: "M" },
+        { nombre: "Sara", nota: 90, sexo: "F" },
+      ];
+       let result = punto6(estudiantes)
+
        expect(result).toBe(result)
-     }),
-     test('lea por teclado dos números, si el primero es mayor al segundo informar su suma y diferencia', () => { 
-        let primero = 3
-        let segundo = 5
-        let result = punto5(primero, segundo)
-        expect(primero).toBeLessThan(segundo);
-        expect(result).toBe(result)
-      })
+       expect(result).toBeDefined()
+       expect(result).toContain("El estudiante con mayor nota fue");
+      expect(result).toContain("El estudiante con menor nota fue");
+     })
      
  });
