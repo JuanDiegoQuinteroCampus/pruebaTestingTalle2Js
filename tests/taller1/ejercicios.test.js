0b5dd196f499f00f8640f3170d6c68bbb2b31a5d
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-01 08:24:44 -0500

    test: :white_check_mark: Testing ejercicio 4
    Se realizó el testing del ejercicio 4, se completo y esta check

--- tests/taller1/ejercicios.test.js
+++ tests/taller1/ejercicios.test.js
@@ -1,12 +1,16 @@
-import { punto3 } from "../../taller1/ejercicios";
+import { punto4 } from "../../taller1/ejercicios";
 
-describe('test punto 3', () => { 
+describe('test punto 4', () => { 
     test('el resultado esperado: El resultado (Voltaje) entre de intensidad multiplicado por resistencia', () => { 
-        let intensidad = 2
-        let resistencia = 5
-        let voltaje = punto3(intensidad, resistencia)
-
-        expect(voltaje).toBe(intensidad * resistencia);
+        const solicitados = [
+            { nombre: "Juan", edad: parseInt(45) },
+            { nombre: "Diego", edad: parseInt(23) },
+            { nombre: "Daniel", edad: parseInt(67) }
+        ];
+        let context = `la persona con nmayor edad es ${solicitados.nombre} con ${solicitados.edad} años.`;
+        let result = punto4(solicitados)
+        expect(result).toBe(context);
+        
      })
      
  });
