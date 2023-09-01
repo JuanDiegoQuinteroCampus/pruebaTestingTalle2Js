b81149b0c717058ff17b7efd6d6ac39ccd698b46
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-01 07:56:52 -0500

    feat: :sparkles: Ejericio 3
    Se realizó el ejercicio 3 del taller2js, ya esta listo para ser testeado

--- taller1/ejercicios.js
+++ taller1/ejercicios.js
@@ -1,19 +1,14 @@
-// 2. Dado un número indicar si es par o impar y si es mayor de 10.
- export const punto2 = (numero) => {
-  let number = parseInt(numero)
-  let resultado = ""
-  if (number % 2 === 0) {
-    resultado = "El número es par";
-  } else {
-    resultado = "El número es impar";
-  }
+// 3. Construir el algoritmo para determinar el voltaje de un
+// circuito a partir de la resistencia y la intensidad de corriente.
 
-  if (number > 10) {
-    resultado += " y mayor que 10";
-  } else {
-    resultado += " y menor o igual a 10";
-  }
 
-  return resultado;
+
+ export const punto3 = (intensidad, resistencia) => {
+  let int = parseInt(intensidad)
+  let resis = parseInt(resistencia)
+  let voltaje = int * resis; 
+
+  return voltaje
+  
 
  }
