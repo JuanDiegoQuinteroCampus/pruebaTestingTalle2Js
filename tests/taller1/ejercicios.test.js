167d77642b33cafce9b86ae4d5025df27ba69ff5
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-01 07:57:39 -0500

    test: :white_check_mark: Testing ejercicio3
    Se realizó el respectivo testing del ejercicio 3 del taller, ya paso el testing

--- tests/taller1/ejercicios.test.js
+++ tests/taller1/ejercicios.test.js
@@ -1,21 +1,12 @@
-import { punto2 } from "../../taller1/ejercicios";
+import { punto3 } from "../../taller1/ejercicios";
 
-describe('test punto 2', () => { 
-    test('el resultado esperado: indicar si el numero es par o impar', () => { 
-        const number = punto2(11);
-        expect(number).toBe("El número es impar y mayor que 10");
-     }),
-     test('el resultado esperado: indicar si el numero es par o impar y si es mayor de 10', () => { 
-        const number = punto2(9);
-        expect(number).toBe("El número es impar y menor o igual a 10");
-     }),
-     test('el resultado esperado: indicar si el numero es par o impar y si es mayor de 10', () => { 
-        const number = punto2(12);
-        expect(number).toBe("El número es par y mayor que 10");
-     }),
-     test('el resultado esperado: indicar si el numero es par o impar y si es mayor de 10', () => { 
-        const number = punto2(6);
-        expect(number).toBe("El número es par y menor o igual a 10");
+describe('test punto 3', () => { 
+    test('el resultado esperado: El resultado (Voltaje) entre de intensidad multiplicado por resistencia', () => { 
+        let intensidad = 2
+        let resistencia = 5
+        let voltaje = punto3(intensidad, resistencia)
+
+        expect(voltaje).toBe(intensidad * resistencia);
      })
      
  });
