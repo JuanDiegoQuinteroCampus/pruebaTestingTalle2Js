503cf81dca027f578ab07ac83f02f700144f84c6
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-01 08:24:07 -0500

    feat: :sparkles: Ejercicio 4
    Se desarrollo el ejercicio 4, dando asi inicio al testing

--- taller1/ejercicios.js
+++ taller1/ejercicios.js
@@ -1,5 +1,37 @@
-// 3. Construir el algoritmo para determinar el voltaje de un
-// circuito a partir de la resistencia y la intensidad de corriente.
+// 4. Construir el algoritmo que solicite el nombre y edad de 3
+// personas y determine el nombre de la persona con mayor edad.
+
+
+export const punto4 =(nombre, edad)=>{
+  let info = [];
+  let info2;
+  let num = 0;
+  let nombreM;
+  let edadM;
+  for (let i = 0; i < 3; i++) {
+       info2 = {
+          nombre: nombre, 
+          edad: parseInt(edad)
+      }
+      info.push(info2);
+  }
+  info.forEach((p)=>{
+   
+      if (p.edad > num) {
+          nombreM = p.nombre;
+          edadM = p.edad;
+          num = edadM;
+      }
+  })
+  let resultado = `la persona con nmayor edad es ${nombreM} con ${edadM} años.`
+  return resultado;
+  
+
+}
+
+
+
+
 
 
 
